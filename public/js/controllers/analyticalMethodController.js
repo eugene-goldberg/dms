9f6dcc8974fb3df29639b6b15cddd4444309873c
eugene <eugene.goldberg117@gmail.com>
2017-06-11 14:14:54 -0500

    added customer selection drop-down.

--- public/js/controllers/analyticalMethodController.js
+++ public/js/controllers/analyticalMethodController.js
@@ -7,12 +7,50 @@ app.controller('analyticalMethodController',['$scope','$http','$odataresource','
             console.log('popping alert');
         }
 
-        $scope.customer_name = "J.L. Webb";
-        $scope.account_key = 7777;
-        $scope.acc1 = 77;
-        $scope.acc2 = 55;
-        $scope.amount = 47;
-        $scope.city = "Chicago";
+        $scope.customer_name = undefined;
+        $scope.account_key = undefined;
+        $scope.acc1 = undefined;
+        $scope.acc2 = undefined;
+        $scope.amount = undefined;
+        $scope.city = undefined;
+
+        $scope.customers = [
+            {"id": 1, "name": "J.L.Webb"},
+            {"id": 2, "name": "P.J.Griffin"}
+        ];
+
+        $scope.selectedCustomer = {};
+
+        $scope.$watch("selectedCustomer", function() {
+            $scope.loading = false;
+            $scope.hideOffer = true;
+            $scope.hideSelectedOffer = true;
+            $scope.hideList = true;
+
+            if ($scope.selectedCustomer.name == "J.L.Webb") {
+                $scope.customer_name = "J.L.Webb";
+                $scope.account_key = 7777;
+                $scope.acc1 = 77;
+                $scope.acc2 = 55;
+                $scope.amount = 47;
+                $scope.city = "Chicago";
+            }
+            if ($scope.selectedCustomer.name == "P.J.Griffin") {
+                $scope.customer_name = "P.J.Griffin";
+                $scope.account_key = 9389;
+                $scope.acc1 = 25;
+                $scope.acc2 = 55;
+                $scope.amount = 37;
+                $scope.city = "Atlanta";
+            }
+        });
+
+        //$scope.customer_name = "J.L. Webb";
+        //$scope.account_key = 7777;
+        //$scope.acc1 = 77;
+        //$scope.acc2 = 55;
+        //$scope.amount = 47;
+        //$scope.city = "Chicago";
 
         $scope.offerID = undefined;
         $scope.loading = false;
